41abd2cfe674900ae9d7f920572df6c8c5a55621
Salimcan Satıcı <135680177+LibreBlocksDev@users.noreply.github.com>
2023-06-06 20:03:48 +0300

    Price-Chart Update
    Temp fix

--- src/views/home/price-chart.tsx
+++ src/views/home/price-chart.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import axios from "axios";
 import {
   LineChart,
   Line,
@@ -9,30 +9,28 @@ import {
   CartesianGrid,
   Tooltip,
   ResponsiveContainer,
-} from 'recharts';
+} from "recharts";
 
-import { Anchor } from 'lucide-react';
+import { Anchor } from "lucide-react";
 
 const Chart = () => {
   const [data, setData] = useState<any[]>([]);
 
-  useEffect(() => {
+  useEffect(() => { //Will updated here  
     const fetchData = async () => {
       const response = await axios.get(
-        'https://api.coingecko.com/api/v3/coins/libre/market_chart?vs_currency=usd&days=7'
+        "https://api.coingecko.com/api/v3/coins/libre/market_chart?vs_currency=usd&days=7"
       );
       const formattedData = [];
       const priceData = response.data.prices;
+     
+      const today = new Date();
+      today.setDate(today.getDate() - 5); // 5 days .. 
+  
       for (let i = 0; i < priceData.length; i++) {
-        const date = new Date(priceData[i][0]).toLocaleDateString('en-US');
-        if (
-          date === '5/30/2023' ||
-          date === '5/31/2023' ||
-          date === '6/1/2023' ||
-          date === '6/2/2023' ||
-          date === '6/3/2023' ||
-          date === '6/4/2023'
-        ) {
+        const date = new Date(priceData[i][0]).toLocaleDateString("en-US");
+        
+        if (date >= `${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`) {
           const price = priceData[i][1];
           formattedData.push({ date, price });
         }
@@ -41,63 +39,73 @@ const Chart = () => {
     };
     fetchData();
   }, []);
+  
   const renderVerticalLines = () => {
     const dateArray = data.map((item) => item.date);
     const lineComponents = [];
     for (let i = 0; i < dateArray.length; i++) {
-      lineComponents.push(<Line key={i} x={dateArray[i]} stroke='#262626' strokeWidth={1} />);
+      lineComponents.push(
+        <Line key={i} x={dateArray[i]} stroke="#262626" strokeWidth={1} />
+      );
     }
     return lineComponents;
   };
 
   return (
-    <div className='w-full min-w-0'>
-      <h4 className='mb-4 text-xl font-semibold text-white'>Price Graph</h4>
-      <div className='divide-y divide-shade-800 rounded-xl border border-shade-800 bg-black text-base text-white'>
-        <div className='p-5'>
-          <div className='mb-5 font-semibold'>Price</div>
-          <ResponsiveContainer width='100%' height={185}>
+    <div className="w-full min-w-0">
+      <h4 className="mb-4 text-xl font-semibold text-white">Price Graph</h4>
+      <div className="divide-y divide-shade-800 rounded-xl border border-shade-800 bg-black text-base text-white">
+        <div className="p-5">
+          <div className="mb-5 font-semibold">Price</div>
+          <ResponsiveContainer width="100%" height={185}>
             <LineChart data={data}>
-              <XAxis dataKey='date' tick={{ fontSize: 12 }} />
+              <XAxis dataKey="date" tick={{ fontSize: 12 }} />
               <YAxis tick={{ fontSize: 12 }} />
-              <CartesianGrid stroke='#262626' vertical={false} />
+              <CartesianGrid stroke="#262626" vertical={false} />
 
               <Tooltip
-                labelStyle={{ fontSize: 14, color: '#fff' }}
-                itemStyle={{ fontSize: 14, color: '#fff' }}
-                contentStyle={{ backgroundColor: '#000', borderColor: '#262626' }}
+                labelStyle={{ fontSize: 14, color: "#fff" }}
+                itemStyle={{ fontSize: 14, color: "#fff" }}
+                contentStyle={{
+                  backgroundColor: "#000",
+                  borderColor: "#262626",
+                }}
               />
               <Line
-                type='monotone'
-                dataKey='price'
-                stroke='#ff6101'
+                type="monotone"
+                dataKey="price"
+                stroke="#ff6101"
                 strokeWidth={1}
-                strokeDasharray='0 0'
+                strokeDasharray="0 0"
                 dot={false}
               />
 
               {renderVerticalLines()}
             </LineChart>
           </ResponsiveContainer>
-          <div className='flex items-center justify-between '>
-            <div className='flex flex-1 items-center space-x-2'>
+          <div className="flex items-center justify-between ">
+            <div className="flex flex-1 items-center space-x-2">
               <span></span>
               <span></span>
             </div>
-            <div className='flex items-center text-xs text-shade-500'>data from CoinGecko API</div>
+            <div className="flex items-center text-xs text-shade-500">
+              data from CoinGecko API
+            </div>
           </div>
         </div>
 
-        <div className='flex items-center justify-between py-4 px-5'>
-          <div className='flex flex-1 items-center space-x-3'>
+        <div className="flex items-center justify-between px-5 py-4">
+          <div className="flex flex-1 items-center space-x-3">
             <span>24h Low / 24h High:</span>
             <span></span>
           </div>
-          <div className='flex items-center'>
-            {' '}
+          <div className="flex items-center">
+            {" "}
             {data.length > 0
-              ? `$${data[0].price.toFixed(7)} / $${data[data.length - 1].price.toFixed(7)}`
-              : '-'}
+              ? `$${data[0].price.toFixed(7)} / $${data[
+                  data.length - 1
+                ].price.toFixed(7)}`
+              : "-"}
           </div>
         </div>
       </div>
