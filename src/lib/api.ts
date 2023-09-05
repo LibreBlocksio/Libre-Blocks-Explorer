eb019444adabfce8b48a155b283fbdc28baafd16
CryptoBloks, LLC <93884436+CryptoBloks@users.noreply.github.com>
2023-09-05 09:09:50 -0700

    consolidation of .env variables

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,42 +2,61 @@ import type { QueryKey } from '@tanstack/react-query';
 import axios from 'axios';
 
 export const getCoinInfo = async () => {
-  const { data } = await axios.get(`${process.env.NEXT_PUBLIC_DASHBOARD_API}/tokens`);
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_LIBRE_API}/tokens`
+  );
   return data;
 };
 
 export const getChainInfo = async () => {
-  const { data } = await axios.get(`${process.env.NEXT_PUBLIC_DASHBOARD_API}/stats/chain`);
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_LIBRE_API}/stats/chain`
+  );
   return data;
 };
 
 export const getExchangeRates = async () => {
-  const { data } = await axios.get(`${process.env.NEXT_PUBLIC_DASHBOARD_API}/exchange-rates`);
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_LIBRE_API}/exchange-rates`
+  );
   return data;
 };
 
-export const getLastTransactions = async ({ queryKey }: { queryKey: QueryKey }) => {
+export const getLastTransactions = async ({
+  queryKey,
+}: {
+  queryKey: QueryKey;
+}) => {
   const [_key, queryParams] = queryKey;
-  const { data } = await axios.get(`${process.env.NEXT_PUBLIC_LIBRE_API}/get_actions`, {
-    params: queryParams,
-  });
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_HISTORY_API}/v2/history/get_actions`,
+    {
+      params: queryParams,
+    }
+  );
 
   return data;
 };
 
 export const getAccount = async ({ queryKey }: { queryKey: QueryKey }) => {
   const [_key, queryParams] = queryKey;
-  const { data } = await axios.get(`${process.env.NEXT_PUBLIC_HYPERION_API}/state/get_account`, {
-    params: queryParams,
-  });
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_HISTORY_API}/v2/state/get_account`,
+    {
+      params: queryParams,
+    }
+  );
   return data;
 };
 
 export const getActions = async ({ queryKey }: { queryKey: QueryKey }) => {
   const [_key, queryParams] = queryKey;
-  const { data } = await axios.get(`${process.env.NEXT_PUBLIC_HYPERION_API}/history/get_actions`, {
-    params: queryParams, //limit=20&skip=5&account=salimcan
-  });
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_HISTORY_API}/v2/history/get_actions`,
+    {
+      params: queryParams, //limit=20&skip=5&account=salimcan
+    }
+  );
   return data;
 };
 
@@ -47,9 +66,12 @@ export const getTransaction = async ({ queryKey }: { queryKey: any }) => {
     return [];
   }
 
-  const { data } = await axios.get(`${process.env.NEXT_PUBLIC_LIBRE_API}/get_transaction`, {
-    params: queryParams,
-  });
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_HISTORY_API}/v2/history/get_transaction`,
+    {
+      params: queryParams,
+    }
+  );
   return data;
 };
 
@@ -59,19 +81,26 @@ export const getBlock = async ({ queryKey }: { queryKey: any }) => {
     return [];
   }
 
-  const { data } = await axios.post(`${process.env.NEXT_PUBLIC_BLOCK_API}/chain/get_block`, {
-    block_num_or_id: queryParams.block_num_or_id,
-  });
+  const { data } = await axios.post(
+    `${process.env.NEXT_PUBLIC_HISTORY_API}/v1/chain/get_block`,
+    {
+      block_num_or_id: queryParams.block_num_or_id,
+    }
+  );
   return data;
 };
 
 export const getProducers = async () => {
-  const { data } = await axios.get(`${process.env.NEXT_PUBLIC_LIBRE_DASHBOARD_API}/producers`);
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_LIBRE_API}/producers`
+  );
   return data;
 };
 
 export const getTokens = async () => {
-  const { data } = await axios.get(`${process.env.NEXT_PUBLIC_DASHBOARD_API}/tokens`);
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_LIBRE_API}/tokens`
+  );
   return data;
 };
 
